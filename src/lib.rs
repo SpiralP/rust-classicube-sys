ee90883fe761e46e567574b391aa6bba46b795d0
SpiralP <spiral.palmer@gmail.com>
2019-10-05 20:32:58 -0700

    doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,42 @@
+/*!
+
+```c
+#include "src/Chat.h"
+#include "src/GameStructs.h"
+
+#ifdef CC_BUILD_WIN
+    #define CC_API __declspec(dllimport)
+    #define CC_VAR __declspec(dllimport)
+    #define EXPORT __declspec(dllexport)
+#else
+    #define CC_API
+    #define CC_VAR
+    #define EXPORT __attribute__((visibility("default")))
+#endif
+
+static void TestPlugin_Init(void) {
+        String msg = String_FromConst("Hello world!");
+        Chat_Add(&msg);
+}
+
+EXPORT int Plugin_ApiVersion = 1;
+EXPORT struct IGameComponent Plugin_Component = { TestPlugin_Init };
+```
+
+```rust
+#[no_mangle]
+pub static Plugin_ApiVersion: c_int = 1;
+
+#[no_mangle]
+pub static Plugin_Component: IGameComponent = IGameComponent {
+  ...
+};
+
+```
+
+
+*/
+
 mod os;
 
 pub use crate::os::*;
-use std::os::raw::c_int;
-
-// __declspec(dllexport) int Plugin_ApiVersion = 1;
-// __declspec(dllexport) struct IGameComponent Plugin_Component = {
-// 	ObjExporter_Init /* Init */
-// };
-
-#[no_mangle]
-pub static Plugin_ApiVersion: c_int = 1;
