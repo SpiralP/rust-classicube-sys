b351fe561fbd4514f3e46fa7db8790acf2b599a6
SpiralP <spiral.palmer@gmail.com>
2019-10-08 15:40:22 -0700

    linux lib file

--- src/lib.rs
+++ src/lib.rs
@@ -28,14 +28,27 @@ EXPORT struct IGameComponent Plugin_Component = { TestPlugin_Init };
 ```
 
 ```rust
+use classicube::*;
+use std::{os::raw::c_int, ptr};
+
 #[no_mangle]
 pub static Plugin_ApiVersion: c_int = 1;
 
 #[no_mangle]
-pub static Plugin_Component: IGameComponent = IGameComponent {
-  ...
+pub static mut Plugin_Component: IGameComponent = IGameComponent {
+  /* Called when the game is being loaded. */
+  Init: None,
+  /* Called when the component is being freed. (e.g. due to game being closed) */
+  Free: None,
+  /* Called to reset the component's state. (e.g. reconnecting to server) */
+  Reset: None,
+  /* Called to update the component's state when the user begins loading a new map. */
+  OnNewMap: None,
+  /* Called to update the component's state when the user has finished loading a new map. */
+  OnNewMapLoaded: None,
+  /* Next component in linked list of components. */
+  next: ptr::null_mut(),
 };
-
 ```
 */
 
