b99471534df30835c22cbf0cd7799e20621ced54
SpiralP <spiral.palmer@gmail.com>
2019-10-09 12:13:46 -0700

    fix doc test

--- src/lib.rs
+++ src/lib.rs
@@ -3,33 +3,13 @@
 #![allow(non_upper_case_globals)]
 
 /*!
-
-```c
-#include "src/Chat.h"
-#include "src/GameStructs.h"
-
-#ifdef CC_BUILD_WIN
-    #define CC_API __declspec(dllimport)
-    #define CC_VAR __declspec(dllimport)
-    #define EXPORT __declspec(dllexport)
-#else
-    #define CC_API
-    #define CC_VAR
-    #define EXPORT __attribute__((visibility("default")))
-#endif
-
-static void TestPlugin_Init(void) {
-        String msg = String_FromConst("Hello world!");
-        Chat_Add(&msg);
-}
-
-EXPORT int Plugin_ApiVersion = 1;
-EXPORT struct IGameComponent Plugin_Component = { TestPlugin_Init };
-```
-
 ```rust
-use classicube::*;
+use classicube_sys::*;
 use std::{os::raw::c_int, ptr};
+
+extern "C" fn init() {
+  // doot
+}
 
 #[no_mangle]
 pub static Plugin_ApiVersion: c_int = 1;
@@ -37,7 +17,7 @@ pub static Plugin_ApiVersion: c_int = 1;
 #[no_mangle]
 pub static mut Plugin_Component: IGameComponent = IGameComponent {
   /* Called when the game is being loaded. */
-  Init: None,
+  Init: Some(init),
   /* Called when the component is being freed. (e.g. due to game being closed) */
   Free: None,
   /* Called to reset the component's state. (e.g. reconnecting to server) */
