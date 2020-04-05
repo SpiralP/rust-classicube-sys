b22c5424ab45f14a5d7e1ff21e976103db649e80
SpiralP <SpiralP@users.noreply.github.com>
2020-04-05 10:46:45 +0000

    Automated deployment: Sun Apr  5 10:46:45 UTC 2020 94210c70e4e3bdac6ad219ff6a878c3b10ddf125

--- implementors/alloc/string/trait.ToString.js
+++ implementors/alloc/string/trait.ToString.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["classicube_sys"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"type\" href=\"classicube_sys/type.String.html\" title=\"type classicube_sys::String\">String</a>","synthetic":false,"types":["classicube_sys::os::linux::String"]}];
+implementors["classicube_sys"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"type\" href=\"classicube_sys/type.String.html\" title=\"type classicube_sys::String\">String</a>","synthetic":false,"types":["classicube_sys::bindings::String"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["arrayvec"] = {"name":"","files":["array.rs","array_string.rs","char.rs","errors.rs","lib.rs","maybe_uninit.rs"]};
-sourcesIndex["classicube_sys"] = {"name":"","dirs":[{"name":"command","files":["mod.rs","owned_chat_command.rs"]},{"name":"os","files":["linux.rs","mod.rs"]},{"name":"vectors","files":["ivec3.rs","mod.rs","vec3.rs"]}],"files":["chat.rs","event.rs","input.rs","lib.rs","packed_col.rs","string.rs","world.rs"]};
+sourcesIndex["classicube_sys"] = {"name":"","dirs":[{"name":"command","files":["mod.rs","owned_chat_command.rs"]},{"name":"vectors","files":["ivec3.rs","mod.rs","vec3.rs"]}],"files":["bindings.rs","chat.rs","event.rs","input.rs","lib.rs","packed_col.rs","string.rs","world.rs"]};
 createSourceSidebar();
