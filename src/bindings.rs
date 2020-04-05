8fa1b6de39eff9e79152cf5f3f56cc1adc1d4597
SpiralP <spiral.palmer@gmail.com>
2020-04-05 03:42:52 -0700

    more clippy ignores

--- src/bindings.rs
+++ src/bindings.rs
@@ -1,4 +1,6 @@
 #![allow(non_snake_case, non_camel_case_types, non_upper_case_globals)]
 #![allow(clippy::missing_safety_doc)]
+#![allow(clippy::unreadable_literal)]
+#![allow(clippy::cognitive_complexity)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
