41c30769d8a5f2b7a1dfcfd6ce8907983987595d
SpiralP <SpiralP@users.noreply.github.com>
2020-04-09 05:21:42 +0000

    deploy: d011240ca90b3853c3833ab716f9480e50dc538b

--- source-files.js
+++ source-files.js
@@ -1,6 +1,6 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["arrayvec"] = {"name":"","files":["array.rs","array_string.rs","char.rs","errors.rs","lib.rs","maybe_uninit.rs"]};
-sourcesIndex["classicube_sys"] = {"name":"","dirs":[{"name":"command","files":["mod.rs","owned_chat_command.rs"]},{"name":"vectors","files":["ivec3.rs","mod.rs","vec3.rs"]}],"files":["bindings.rs","chat.rs","event.rs","input.rs","lib.rs","packed_col.rs","string.rs","world.rs"]};
+sourcesIndex["classicube_sys"] = {"name":"","dirs":[{"name":"command","files":["mod.rs","owned_chat_command.rs"]},{"name":"vectors","files":["ivec3.rs","mod.rs","vec3.rs"]}],"files":["bindings.rs","chat.rs","event.rs","input.rs","lib.rs","model.rs","packed_col.rs","string.rs","world.rs"]};
 sourcesIndex["paste"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["paste_impl"] = {"name":"","files":["enum_hack.rs","lib.rs"]};
 sourcesIndex["proc_macro2"] = {"name":"","files":["fallback.rs","lib.rs","strnom.rs","wrapper.rs"]};
