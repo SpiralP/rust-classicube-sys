aae700c33d635b42df31926832de7a046a7493d2
SpiralP <spiral.palmer@gmail.com>
2020-04-09 02:14:26 -0700

    make OwnedChatCommand pinned, add register() method

--- src/command/owned_chat_command.rs
+++ src/command/owned_chat_command.rs
@@ -1,45 +1,72 @@
-use crate::ChatCommand;
-use arrayvec::ArrayVec;
-use std::{ffi::CString, os::raw::c_int, ptr};
+use crate::{bindings::Commands_Register, ChatCommand};
+use pin_project::{pin_project, project};
+use std::{ffi::CString, os::raw::c_int, pin::Pin, ptr};
 
+#[pin_project]
 pub struct OwnedChatCommand {
+    #[pin]
     pub name: CString,
+    #[pin]
     pub help: Vec<CString>,
+    #[pin]
     pub command: ChatCommand,
 }
 
 impl OwnedChatCommand {
     pub fn new(
-        name: &'static str,
+        name: &str,
         execute: unsafe extern "C" fn(args: *const crate::String, argsCount: c_int),
         singleplayer_only: bool,
-        mut help: Vec<&'static str>,
-    ) -> Self {
+        mut help: Vec<&str>,
+    ) -> Pin<Box<OwnedChatCommand>> {
         let name = CString::new(name).unwrap();
 
         let help: Vec<CString> = help.drain(..).map(|s| CString::new(s).unwrap()).collect();
+
+        let help_array = [
+            help.get(0).map(|cs| cs.as_ptr()).unwrap_or(ptr::null()),
+            help.get(1).map(|cs| cs.as_ptr()).unwrap_or(ptr::null()),
+            help.get(2).map(|cs| cs.as_ptr()).unwrap_or(ptr::null()),
+            help.get(3).map(|cs| cs.as_ptr()).unwrap_or(ptr::null()),
+            help.get(4).map(|cs| cs.as_ptr()).unwrap_or(ptr::null()),
+        ];
 
         let command = ChatCommand {
             Name: name.as_ptr(),
             Execute: Some(execute),
             SingleplayerOnly: if singleplayer_only { 1 } else { 0 },
-            Help: {
-                let mut array: ArrayVec<[*const ::std::os::raw::c_char; 5usize]> =
-                    help.iter().map(|cstr| cstr.as_ptr()).collect();
-
-                while !array.is_full() {
-                    array.push(ptr::null());
-                }
-
-                array.into_inner().unwrap()
-            },
+            Help: help_array,
             next: ptr::null_mut(),
         };
 
-        Self {
+        Box::pin(Self {
             name,
             help,
             command,
+        })
+    }
+}
+
+#[project]
+impl OwnedChatCommand {
+    #[allow(dead_code)]
+    #[project]
+    pub fn register(&mut self) {
+        #[project]
+        let OwnedChatCommand { command, .. } = self;
+
+        unsafe {
+            Commands_Register(command.as_mut().get_unchecked_mut());
         }
     }
+}
+
+#[ignore]
+#[test]
+fn test_owned_chat_command() {
+    extern "C" fn c_command_callback(_args: *const crate::String, _args_count: c_int) {}
+
+    let mut cmd = OwnedChatCommand::new("Roll", c_command_callback, false, vec![]);
+
+    cmd.as_mut().project().register();
 }
