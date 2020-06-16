a51d1e8ef729b43efb866d35a8882e4c200999aa
SpiralP <SpiralP@users.noreply.github.com>
2020-06-16 03:07:07 +0000

    deploy: 69912e04b866d0b23a13dbc2e3783c956f80d372

--- source-files.js
+++ source-files.js
@@ -1,10 +1,6 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["classicube_sys"] = {"name":"","dirs":[{"name":"command","files":["mod.rs","owned_chat_command.rs"]},{"name":"graphics","files":["mod.rs","owned_texture.rs","owned_vertex_buffer.rs"]},{"name":"gui","files":["mod.rs","screen.rs"]},{"name":"vectors","files":["ivec3.rs","mod.rs","vec3.rs"]}],"files":["bindings.rs","chat.rs","entity.rs","event.rs","input.rs","inventory.rs","lib.rs","model.rs","packed_col.rs","string.rs","world.rs"]};
 sourcesIndex["paste"] = {"name":"","files":["lib.rs"]};
-sourcesIndex["paste_impl"] = {"name":"","files":["enum_hack.rs","lib.rs"]};
-sourcesIndex["proc_macro2"] = {"name":"","files":["detection.rs","fallback.rs","lib.rs","parse.rs","wrapper.rs"]};
+sourcesIndex["paste_impl"] = {"name":"","files":["enum_hack.rs","error.rs","lib.rs"]};
 sourcesIndex["proc_macro_hack"] = {"name":"","files":["error.rs","iter.rs","lib.rs","parse.rs","quote.rs"]};
-sourcesIndex["quote"] = {"name":"","files":["ext.rs","format.rs","ident_fragment.rs","lib.rs","runtime.rs","spanned.rs","to_tokens.rs"]};
-sourcesIndex["syn"] = {"name":"","dirs":[{"name":"gen","files":["gen_helper.rs"]}],"files":["attr.rs","bigint.rs","buffer.rs","custom_keyword.rs","custom_punctuation.rs","data.rs","derive.rs","discouraged.rs","error.rs","export.rs","expr.rs","ext.rs","generics.rs","group.rs","ident.rs","lib.rs","lifetime.rs","lit.rs","lookahead.rs","mac.rs","macros.rs","op.rs","parse.rs","parse_macro_input.rs","parse_quote.rs","path.rs","print.rs","punctuated.rs","sealed.rs","span.rs","spanned.rs","thread.rs","token.rs","ty.rs","verbatim.rs"]};
-sourcesIndex["unicode_xid"] = {"name":"","files":["lib.rs","tables.rs"]};
 createSourceSidebar();
