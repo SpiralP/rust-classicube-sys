b89e2975f25b317b4c4a9a59d7069a3afababd1a
SpiralP <SpiralP@users.noreply.github.com>
2020-06-26 03:28:24 +0000

    deploy: 621125f783ed248337edebf73c8b15e224b39255

--- source-files.js
+++ source-files.js
@@ -1,5 +1,5 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["classicube_sys"] = {"name":"","dirs":[{"name":"command","files":["mod.rs","owned_chat_command.rs"]},{"name":"graphics","files":["mod.rs","owned_texture.rs","owned_vertex_buffer.rs"]},{"name":"gui","files":["mod.rs","screen.rs"]}],"files":["bindings.rs","chat.rs","constants.rs","entity.rs","event.rs","input.rs","inventory.rs","lib.rs","model.rs","packed_col.rs","string.rs","vectors.rs","world.rs"]};
+sourcesIndex["classicube_sys"] = {"name":"","dirs":[{"name":"command","files":["mod.rs","owned_chat_command.rs"]},{"name":"graphics","files":["mod.rs","owned_texture.rs","owned_vertex_buffer.rs"]},{"name":"gui","files":["mod.rs","screen.rs"]},{"name":"vectors","files":["mod.rs","ops.rs"]}],"files":["bindings.rs","chat.rs","constants.rs","entity.rs","event.rs","input.rs","inventory.rs","lib.rs","math.rs","model.rs","packed_col.rs","particle.rs","string.rs","world.rs"]};
 sourcesIndex["paste"] = {"name":"","files":["lib.rs"]};
 sourcesIndex["paste_impl"] = {"name":"","files":["enum_hack.rs","error.rs","lib.rs"]};
 sourcesIndex["proc_macro_hack"] = {"name":"","files":["error.rs","iter.rs","lib.rs","parse.rs","quote.rs"]};
