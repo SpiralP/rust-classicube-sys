909cf195ca77d797842461fd6c144c958459b704
SpiralP <SpiralP@users.noreply.github.com>
2020-11-28 03:36:51 +0000

    deploy: 4666c6c62f827387abe94fe6fd7d994813aa54a6

--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex["classicube_sys"] = {"name":"","dirs":[{"name":"command","files":["mod.rs","owned_chat_command.rs"]},{"name":"graphics","files":["mod.rs","owned_texture.rs","owned_vertex_buffer.rs"]},{"name":"gui","files":["mod.rs","screen.rs"]},{"name":"vectors","files":["mod.rs","ops.rs"]}],"files":["bindings.rs","chat.rs","constants.rs","entity.rs","event.rs","input.rs","inventory.rs","lib.rs","math.rs","model.rs","packed_col.rs","particle.rs","string.rs","world.rs"]};
-sourcesIndex["paste"] = {"name":"","files":["doc.rs","error.rs","lib.rs"]};
+sourcesIndex["paste"] = {"name":"","files":["attr.rs","error.rs","lib.rs","segment.rs"]};
 createSourceSidebar();
