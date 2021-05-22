0180a5bd3abb5a51455a67c31ec48c8135d32da0
SpiralP <spiral.palmer@gmail.com>
2021-05-22 05:59:07 -0700

    convert between code page 437 and rust String

--- src/string.rs
+++ src/string.rs
@@ -1,4 +1,4 @@
-use crate::bindings::{cc_string, cc_uint16, STRING_SIZE};
+use crate::bindings::{cc_codepoint, cc_string, cc_uint16, cc_unichar, STRING_SIZE};
 use std::{
     borrow::Borrow,
     ffi::CString,
@@ -17,7 +17,14 @@ impl cc_string {
 
 impl ToString for cc_string {
     fn to_string(&self) -> String {
-        String::from_utf8_lossy(self.as_slice()).to_string()
+        String::from_utf16_lossy(
+            &self
+                .as_slice()
+                .iter()
+                .map(|c| Convert_CP437ToUnicode(*c))
+                .collect::<Vec<_>>(),
+        )
+        .to_string()
     }
 }
 
@@ -33,12 +40,18 @@ pub struct OwnedString {
 }
 
 impl OwnedString {
-    pub fn new<S: Into<Vec<u8>>>(s: S) -> Self {
-        let chars = s.into();
-        let length = chars.len();
-        let capacity = chars.len();
+    pub fn new<S: Into<String>>(s: S) -> Self {
+        let bytes = s
+            .into()
+            // TODO is chars() "codepoints" in cc's definition?
+            .chars()
+            .map(|c| c as cc_codepoint)
+            .map(Convert_CodepointToCP437)
+            .collect::<Vec<_>>();
+        let length = bytes.len();
+        let capacity = bytes.len();
 
-        let mut c_string = Box::pin(CString::new(chars).unwrap());
+        let mut c_string = Box::pin(CString::new(bytes).unwrap());
         let buffer: *const c_char = unsafe { c_string.as_mut().get_unchecked_mut().as_ptr() };
 
         Self {
@@ -114,6 +127,114 @@ fn test_get_string() {
         assert_eq!(
             UNSAFE_GetString(&s).to_string(),
             "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijkl"
+        );
+    }
+}
+
+pub const controlChars: &[cc_unichar] = &[
+    0x0000, 0x263A, 0x263B, 0x2665, 0x2666, 0x2663, 0x2660, 0x2022, 0x25D8, 0x25CB, 0x25D9, 0x2642,
+    0x2640, 0x266A, 0x266B, 0x263C, 0x25BA, 0x25C4, 0x2195, 0x203C, 0x00B6, 0x00A7, 0x25AC, 0x21A8,
+    0x2191, 0x2193, 0x2192, 0x2190, 0x221F, 0x2194, 0x25B2, 0x25BC,
+];
+
+pub const extendedChars: &[cc_unichar] = &[
+    0x2302, 0x00C7, 0x00FC, 0x00E9, 0x00E2, 0x00E4, 0x00E0, 0x00E5, 0x00E7, 0x00EA, 0x00EB, 0x00E8,
+    0x00EF, 0x00EE, 0x00EC, 0x00C4, 0x00C5, 0x00C9, 0x00E6, 0x00C6, 0x00F4, 0x00F6, 0x00F2, 0x00FB,
+    0x00F9, 0x00FF, 0x00D6, 0x00DC, 0x00A2, 0x00A3, 0x00A5, 0x20A7, 0x0192, 0x00E1, 0x00ED, 0x00F3,
+    0x00FA, 0x00F1, 0x00D1, 0x00AA, 0x00BA, 0x00BF, 0x2310, 0x00AC, 0x00BD, 0x00BC, 0x00A1, 0x00AB,
+    0x00BB, 0x2591, 0x2592, 0x2593, 0x2502, 0x2524, 0x2561, 0x2562, 0x2556, 0x2555, 0x2563, 0x2551,
+    0x2557, 0x255D, 0x255C, 0x255B, 0x2510, 0x2514, 0x2534, 0x252C, 0x251C, 0x2500, 0x253C, 0x255E,
+    0x255F, 0x255A, 0x2554, 0x2569, 0x2566, 0x2560, 0x2550, 0x256C, 0x2567, 0x2568, 0x2564, 0x2565,
+    0x2559, 0x2558, 0x2552, 0x2553, 0x256B, 0x256A, 0x2518, 0x250C, 0x2588, 0x2584, 0x258C, 0x2590,
+    0x2580, 0x03B1, 0x00DF, 0x0393, 0x03C0, 0x03A3, 0x03C3, 0x00B5, 0x03C4, 0x03A6, 0x0398, 0x03A9,
+    0x03B4, 0x221E, 0x03C6, 0x03B5, 0x2229, 0x2261, 0x00B1, 0x2265, 0x2264, 0x2320, 0x2321, 0x00F7,
+    0x2248, 0x00B0, 0x2219, 0x00B7, 0x221A, 0x207F, 0x00B2, 0x25A0, 0x00A0,
+];
+
+pub fn Convert_CP437ToUnicode(raw: u8) -> cc_unichar {
+    if raw < 0x20 {
+        controlChars[raw as usize]
+    } else if raw < 0x7F {
+        raw as cc_unichar
+    } else {
+        extendedChars[raw as usize - 0x7F]
+    }
+}
+
+pub fn Convert_CodepointToCP437(cp: cc_codepoint) -> u8 {
+    let mut c: u8 = 0;
+    Convert_TryCodepointToCP437(cp, &mut c);
+    c
+}
+
+fn ReduceEmoji(cp: cc_codepoint) -> cc_codepoint {
+    if cp == 0x1F31E {
+        return 0x263C;
+    }
+    if cp == 0x1F3B5 {
+        return 0x266B;
+    }
+    if cp == 0x1F642 {
+        return 0x263A;
+    }
+
+    if cp == 0x1F600 || cp == 0x1F601 || cp == 0x1F603 {
+        return 0x263A;
+    }
+    if cp == 0x1F604 || cp == 0x1F606 || cp == 0x1F60A {
+        return 0x263A;
+    }
+    cp
+}
+
+fn Convert_TryCodepointToCP437(mut cp: cc_codepoint, c: &mut u8) -> bool {
+    if cp >= 0x20 && cp < 0x7F {
+        *c = cp as u8;
+        return true;
+    }
+    if cp >= 0x1F000 {
+        cp = ReduceEmoji(cp);
+    }
+
+    for i in 0..controlChars.len() {
+        if controlChars[i] as cc_codepoint == cp {
+            *c = i as u8;
+            return true;
+        }
+    }
+
+    for i in 0..extendedChars.len() {
+        if extendedChars[i] as cc_codepoint == cp {
+            *c = (i + 0x7F) as u8;
+            return true;
+        }
+    }
+
+    *c = b'?';
+    return false;
+}
+
+#[test]
+fn test_cp_437_conversion() {
+    let bytes: &[u8] = &[97, 236, 236]; // "a∞∞"
+
+    let c_str = CString::new(bytes).unwrap();
+    let a = cc_string {
+        buffer: c_str.as_ptr() as *mut c_char,
+        length: bytes.len() as cc_uint16,
+        capacity: bytes.len() as cc_uint16,
+    };
+    assert_eq!(a.to_string(), "a∞∞");
+
+    let str = "a∞∞";
+    let s = OwnedString::new(str);
+    unsafe {
+        assert_eq!(
+            slice::from_raw_parts(
+                s.as_cc_string().buffer as *const u8,
+                s.as_cc_string().length as usize
+            ),
+            bytes
         );
     }
 }
