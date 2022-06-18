b2175b47248e7d6a9febf44f5067e72eacee0406
SpiralP <SpiralP@users.noreply.github.com>
2022-06-18 20:19:22 +0000

    deploy: a749c2af60e84381ae2b71cdc28f8a67ddd92153

--- implementors/alloc/string/trait.ToString.js
+++ implementors/alloc/string/trait.ToString.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["classicube_sys"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.60.0/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"type\" href=\"classicube_sys/type.cc_string.html\" title=\"type classicube_sys::cc_string\">cc_string</a>","synthetic":false,"types":["classicube_sys::bindings::cc_string"]}];
+implementors["classicube_sys"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.61.0/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"type\" href=\"classicube_sys/type.cc_string.html\" title=\"type classicube_sys::cc_string\">cc_string</a>","synthetic":false,"types":["classicube_sys::bindings::cc_string"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
