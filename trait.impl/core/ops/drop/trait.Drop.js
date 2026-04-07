686003f3257d1b804f36bb7cec2b758f74158091
SpiralP <SpiralP@users.noreply.github.com>
2026-04-07 22:27:33 +0000

    deploy: 1ef7f172f0658781d861f18b78cf803bbb6662ae

---
+++ trait.impl/core/ops/drop/trait.Drop.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["classicube_sys",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"classicube_sys/screen/struct.OwnedScreen.html\" title=\"struct classicube_sys::screen::OwnedScreen\">OwnedScreen</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"classicube_sys/struct.OwnedGfxTexture.html\" title=\"struct classicube_sys::OwnedGfxTexture\">OwnedGfxTexture</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"classicube_sys/struct.OwnedGfxVertexBuffer.html\" title=\"struct classicube_sys::OwnedGfxVertexBuffer\">OwnedGfxVertexBuffer</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[920]}
